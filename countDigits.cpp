8b510d8ea068f7814a24f7de9d3c547717464e73
Chinmay8897 <149857629+Chinmay8897@users.noreply.github.com>
2024-06-04 23:09:22 +0530

    Add files via upload

---
+++ countDigits.cpp
@@ -0,0 +1,20 @@
+class Solution{
+public:
+    int evenlyDivides(int N){
+        //code here
+        int n=N,c=0;
+        while(n>0)
+        {
+            if((n%10)==0)
+            {n=n/10;
+            continue;
+            }
+            if(N%(n%10)==0)
+            c++;
+            n=n/10;
+            
+            
+        }
+        return c;
+    }
+};
